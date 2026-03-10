5c48045801344ee2cc986e27ae1f483cf57bb379
edxzyluksz <edxzyluksz@gmail.com>
2026-03-10 16:13:30 -0300

    v3 - Backup de Arquivos

--- cronometro/script.js
+++ cronometro/script.js
@@ -6,7 +6,22 @@ let timer = 0;
 let voltas = [];
 
 const formatTime = (time) => {
-    const hours = Math.floor(time / 360000);
-    const minutes = Math.floor(time / 360000)/6000;
-    const minute = Math.floor(time / 6000) / 100;
+    const horas = Math.floor(time / 360000);
+    const minutos = Math.floor(time / 360000)/6000;
+    const segundos = Math.floor(time / 6000) / 100;
+    const milesimos = time % 100;
+
+    return `${horas.toString().padStart(2,'0')}:${minutos.toString().padStart(2,'0')}:${segundos.toString().padStart(2,'0')}:${milesimos.toString().padStart(2,'0')}`
+}
+
+const toggleTimer = () => {
+    const button = document.getElementById('power');
+    const action = document.getAttribute('action');
+    
+    clearInterval(intervalId);
+
+    if (action =='start' || action =='continue' )
+        intervalid = setInterval(() =>
+    )
+    
 }
